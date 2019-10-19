db9c41f0690f3799c26b9217af8908ab0d565681
Bowen Zheng <bowenzheng998@gmail.com>
2019-10-20 01:52:11 +1300

    Added press down animations to schedule items

--- lib/widgets/schedule_item.dart
+++ lib/widgets/schedule_item.dart
@@ -4,6 +4,7 @@ import 'package:flutter/widgets.dart';
 import 'package:fonetracker/models/race.dart';
 import 'package:fonetracker/screens/race_page.dart';
 import 'package:fonetracker/utils/router.dart';
+import 'package:fonetracker/widgets/homepage_item.dart';
 import 'package:fonetracker/widgets/list_item.dart';
 
 class ScheduleItem extends StatefulWidget{
@@ -21,40 +22,46 @@ class ScheduleItem extends StatefulWidget{
 class _ScheduleItemState extends State<ScheduleItem> with TickerProviderStateMixin{
 
   AnimationController controller;
+  VoidCallback onPressed;
 
   @override
   void initState() {
     super.initState();
+    onPressed = () =>  Navigator.of(context).push<void>(CupertinoPageRoute(
+      builder: (context) => RacePage(race: widget.race,),
+      fullscreenDialog: true,
+    ));
     controller = AnimationController(
         duration: const Duration(milliseconds: 500), vsync: this);
   }
 
   @override
   Widget build(BuildContext context) {
-    return CustomListItem(
-      padding: 4.0,
-      radius: Radius.circular(8.0),
-      blurRadius: 0.8,
-      height: 55.0,
-      child: GestureDetector(
-        behavior: HitTestBehavior.translucent,
-        child: Padding(
-          padding: const EdgeInsets.all(16.0  ),
-          child: Row(
-            children: <Widget>[
-              Text(widget.race.raceName),
-              Spacer(),
-              Text("Round " +  widget.race.round.toString()),
-              SizedBox(width: 5.0,),
-            ],
+    return Padding(
+      padding: const EdgeInsets.all(6.0),
+      child: PressableCard(
+        borderRadius: const BorderRadius.all(Radius.circular(8.0)),
+        upElevation: 2,
+        downElevation: 1,
+        onPressed: () =>  Navigator.of(context).push<void>(CupertinoPageRoute(
+          builder: (context) => RacePage(race: widget.race,),
+          fullscreenDialog: true,
+        )),
+        child: Container(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(16.0, 16.0, 8.0, 16.0),
+            child: Row(
+              children: <Widget>[
+                Text(widget.race.raceName),
+                Spacer(),
+                Text("Round " +  widget.race.round.toString()),
+                SizedBox(width: 5.0,),
+                Icon(Icons.more_vert),
+              ],
+            ),
           ),
         ),
-        onTap: (){
-          Navigator.of(context).push<void>(CupertinoPageRoute(
-            builder: (context) => RacePage(race: widget.race,),
-            fullscreenDialog: true,
-          ));
-        },
       ),
     );
   }
